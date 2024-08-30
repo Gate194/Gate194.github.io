64accc268389fdd6f917bd14d37fe892675cb338
Gate194 <159948848+Gate194@users.noreply.github.com>
2024-08-30 10:42:43 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function showSection(sectionId) {
     // Ocultar todas las secciones
     document.getElementById('peliculas').classList.remove('active');
     document.getElementById('series').classList.remove('active');
-    
+    // Puedes agregar más secciones aquí si es necesario
+
     // Mostrar la sección seleccionada
     document.getElementById(sectionId).classList.add('active');
 }
